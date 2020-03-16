82b2783032fa0b347ac9b8dd65d9d4706da1832f
Nekuskus <myangowski@protonmail.com>
2020-03-16 14:33:05 +0100

    First steps

---
+++ Fibonacci.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czas teraz to " + DateTime.Now);
+            Console.WriteLine("Czy chciałbyś obliczyć ciag Fibonacciego czy Tribonacciego?");
+            int branch = Console.ReadLine;
+        }
+        static bool Fibonacci()
+        {
+
+        }
+        static bool Tribonacci()
+        {
+
+        }
+    }
+}
