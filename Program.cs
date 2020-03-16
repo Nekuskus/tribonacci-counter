3e15992921cc402ed7453068dbaaba3c14742f40
Nekuskus <myangowski@protonmail.com>
2020-03-16 15:34:09 +0100

    First Fibonacci try

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Fibonacci
+namespace Program
 {
     class Fibonacci
     {
@@ -11,7 +11,7 @@ namespace Fibonacci
             do
             {
                 Console.WriteLine("//Mozliwe odpowiedzi: 2, 3");
-                int branch = Convert.ToInt16(Console.ReadLine);
+                int branch = Convert.ToInt32(Console.ReadLine);
                 if(branch == 2)
                 {
                     Fibonacci();
@@ -24,11 +24,51 @@ namespace Fibonacci
                 {
                     Console.WriteLine($"Niepoprawna odpowiedz: {branch}");
                 }
-            } while (branch = null);
+            } while ((branch != 2) || (branch != 3));
         }
         static bool Fibonacci()
         {
-
+            List<int> ciag = new List<int>();
+            Console.WriteLine("Czy zmodyfikować pierwsze dwie liczby?\n(Eksperymentalne)");
+            do
+            {
+                Console.WriteLine("//Mozliwe odpowiedzi: Tak, Nie");
+                string modify = Console.ReadLine();
+                if(modify == "Tak")
+                {
+                    //tekst
+                    ciag[0]= Convert.ToInt32(Console.ReadLine());
+                    //tekst
+                    ciag[1] = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                else if(modify == "Nie")
+                {
+                    ciag[0] = 0;
+                    ciag[1] = 1;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Niepoprawna odpowiedz: {modify}");
+                }
+            } while ((modify != "Nie") || (modify != "Tak"));
+            Console.WriteLine("Do ktorej liczby wyliczac ciag?");
+            try
+            {
+                int limit = Convert.ToInt32(Console.ReadLine);
+            } catch (InvalidCastException)
+            {
+                Console.WriteLine("Niewlasciwa odpowiedz");
+            }
+            for(i = 0; i <= limit; i++)
+            {
+                Console.Write($"{i+1} - {ciag[i]}");
+                if(i>1)
+                {
+                    ciag[i] = ciag[i-1] + ciag[i-2];
+                }
+            }
         }
         static bool Tribonacci()
         {
